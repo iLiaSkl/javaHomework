6b7231d55872a0b64510fa55de5f26c8c35f781f
Ilia Skliaruk <ilia.skliaruk2@edu.lapinamk.fi>
2019-12-03 19:42:21 +0200

    Fix hw 5_1

--- 5_1/Sairaala/src/Main.java
+++ 5_1/Sairaala/src/Main.java
@@ -1,23 +1,28 @@
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
 
 
 public class Main {
+    private static final int TEMP_MAX = 40;
+    private static final int TEMP_MIN = 32;
+    private static final int PATIENTS = 30;
+    private static final double HEALTHY_MIN = 36.2;
+    private static final double HEALTHY_MAX = 36.9;
+
     public static void main(String[] args) {
-       Float[] temperatures = new Float[30];
-       for (int i = 0; i < temperatures.length; i++)
-       {
-           temperatures[i] = ((float)  ( Math.random() * 8) - 7 + 39);
-           System.out.print((temperatures[i]) + "\t");
-       }
-       System.out.println("");
-       long numberOfHealthPeople = Arrays.stream(temperatures).filter(i -> i >= 36.2 && i <= 36.9).count();
-       System.out.println(numberOfHealthPeople);
 
-       Double average = Arrays.stream(temperatures).collect(Collectors.averagingDouble(value -> value));
+        double[] temperatures = DoubleStream.generate(() ->
+                Math.random() * (TEMP_MAX - TEMP_MIN) + TEMP_MIN).limit(PATIENTS).toArray();
+        long numberOfHealthPeople = Arrays.stream(temperatures).filter(i -> i >= HEALTHY_MIN && i <= HEALTHY_MAX).count();
+        System.out.println(numberOfHealthPeople);
 
-       System.out.printf("Average temperature: %3.1f", average);
+        Double average = Arrays.stream(temperatures)
+                .average()
+                .orElse(0);
+
+        System.out.printf("Average temperature: %3.1f", average);
     }
 }
 
