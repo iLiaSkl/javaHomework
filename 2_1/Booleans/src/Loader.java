bda0d7182073312ab867389f87ac74b60196b61e
Ilia Skliaruk <ilia.skliaruk2@edu.lapinamk.fi>
2019-12-07 09:54:27 +0200

    Fix 2_1 Booleans

--- 2_1/Booleans/src/Loader.java
+++ 2_1/Booleans/src/Loader.java
@@ -3,41 +3,33 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Integer milkAmount = 200; // ml
-        Integer powderAmount = 300; // g
+        Integer milkAmount = 1200; // ml
+        Integer powderAmount = 400; // g
         Integer eggsCount = 4; // items
-        Integer sugarAmount = 5; // g
+        Integer sugarAmount = 15; // g
         Integer oilAmount = 30; // ml
-        Integer appleCount = 8;
+        Integer appleCount = 38;
+        String text;
 
         //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30)
-        {
-            System.out.println("You could make a Pancakes");
-        } else
-        {
-            System.out.println("U dont have enough for pancakes");
-        }
+        String pancakes = powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30 ? "You could make a Pancakes"
+        : "U dont have enough for pancakes";
+        System.out.println(pancakes);
+
 
 
         //milk - 300 ml, powder - 5 g, eggs - 5
-        if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5)
-        {
-            System.out.println("You could make an Omelette");
-        } else
-        {
-            System.out.println("U dont have enough for omelette");
-        }
+        String omelette = milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5 ? "You could make an Omelette"
+                : "U dont have enough for omelette";
+
+        System.out.println(omelette);
 
 
         //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4)
-        {
-            System.out.println("You could make an Apple pie");
-        } else
-        {
-            System.out.println("U dont have enough for apple pie");
-        }
+        String apple = appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4 ? "You could make an Apple pie"
+        : "U dont have enough for apple pie";
+        System.out.println(apple);
+
 
     }
 }
